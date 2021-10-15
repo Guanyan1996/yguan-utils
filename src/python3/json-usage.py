7ca9d4d7075d86f9e6221c467ae34472b6f466f7
Guanyan1996 <gy_1996@hotmail.com>
2021-10-15 14:28:38 +0800

    【update】增加json的压缩方式

--- src/python3/json-usage.py
+++ src/python3/json-usage.py
@@ -1,6 +1,9 @@
+import gzip
 import json
 from pathlib import Path
 from typing import List, Dict, Tuple, Hashable, Any
+
+import brotli
 
 
 def raise_on_duplicate_keys(ordered_pairs: List[Tuple[Hashable, Any]]) -> Dict:
@@ -17,3 +20,20 @@ def raise_on_duplicate_keys(ordered_pairs: List[Tuple[Hashable, Any]]) -> Dict:
 def check_duplicate_keys(file):
     """检查json文件中是否存在重复的key，原hook默认为update"""
     return json.load(Path(file).open('r'), object_pairs_hook=raise_on_duplicate_keys)
+
+
+# brotli的压缩速度很慢，但是解压速度比gzip快很多，动态文件压缩的话，用gzip，静态文件用brotli
+def compress_json_gzip(jsonfilename):
+    data = {}
+    with gzip.open(jsonfilename, 'wt', encoding='UTF-8') as zipfile:
+        json.dump(data, zipfile)
+
+    with gzip.open(jsonfilename, 'rt', encoding='UTF-8') as zipfile:
+        my_object = json.load(zipfile)
+
+
+def compress_json_brotli(jsonfilename):
+    data = json.dumps(json.load(open(jsonfilename, 'r')), separators=(',', ':')).encode('utf-8')
+    with open("test.BR", 'wb') as f:
+        content = brotli.compress(data, quality=11)
+        f.write(content)
