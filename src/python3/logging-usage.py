1a40d44150d8670da10984c913391660f64c5364
Guanyan1996 <gy_1996@hotmail.com>
2022-03-08 14:52:34 +0800

    【update】增加同时存储文件和输出log的logging默认模块用法

---
+++ src/python3/logging-usage.py
@@ -0,0 +1,18 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s: - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+fh = logging.FileHandler('log.txt')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
